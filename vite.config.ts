b8f851e9e20998307ce769990b595eceb8f47d2d
cjnghn <chotnt741@gmail.com>
2025-03-04 00:51:18 +0900

    add: deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/tabmatrix/',  // gh-pages 설정
   plugins: [react(), tailwindcss()],
 })
